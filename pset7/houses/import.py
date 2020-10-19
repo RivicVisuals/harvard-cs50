c2ed122e13729641c4f4056b369f399cce4f24ca
Luka Rivić <luka@rivic.at>
2020-10-19 15:50:48 +0200

    fixing empty case for middle name

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -26,7 +26,7 @@ with open(argv[1], "r") as fin:
 
         # if a person has no middle name, insert it as NULL
         if len(name) < 3:
-            name.insert(1, "")
+            name.insert(1, None)
 
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)",
                    name[0], name[1], name[2], house, birth)
